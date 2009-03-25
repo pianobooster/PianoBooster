19a014e4c7b94187c8847a4e6af31497ec7d40ae
louisjb <louisjb@ba081f5d-443b-49a7-ac4b-446c3f91f371>
2009-03-25 23:07:46 +0000

    Added repeat Bars
    git-svn-id: https://svn.code.sf.net/p/pianobooster/code/trunk@42 ba081f5d-443b-49a7-ac4b-446c3f91f371

---
+++ PianoBooster/src/GuiLoopingPopup.cpp
@@ -0,0 +1,77 @@
+/*!
+    @file           GuiLoopingPopup.cpp
+
+    @brief          xxx.
+
+    @author         L. J. Barman
+
+    Copyright (c)   2008, L. J. Barman, all rights reserved
+
+    This file is part of the PianoBooster application
+
+    PianoBooster is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PianoBooster is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PianoBooster.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <QtGui>
+
+#include "GuiLoopingPopup.h"
+#include "GlView.h"
+
+GuiLoopingPopup::GuiLoopingPopup(QWidget *parent)
+    : QWidget(parent)
+{
+    setupUi(this);
+    m_song = 0;
+    setWindowTitle("Continious Looping");
+    //fixme setModal(false);
+    setWindowFlags(Qt::Popup);
+}
+
+
+void GuiLoopingPopup::init(CSong* song)
+{
+    m_song = song;
+    loopBarsSpin->setValue(m_song->getLoopingBars());
+    updateInfo();
+}
+
+void GuiLoopingPopup::updateInfo()
+{
+    if (m_song->getLoopingBars() > 0.0)
+        loopingText->setText("Repeat End Bar: " + QString().setNum(m_song->getPlayUptoBar()));
+    else
+        loopingText->setText("Repeat Bar is disabled");
+}
+
+
+void GuiLoopingPopup::on_loopBarsSpin_valueChanged(double bars)
+{
+    if (!m_song) return;
+
+    // Stop the muisc playing
+    m_song->playMusic(false);
+    //fixme setPlayButtonState(false);
+
+    m_song->setLoopingBars( bars);
+    updateInfo();
+}
+
+void GuiLoopingPopup::closeEvent(QCloseEvent *event)
+{
+    QPushButton* parent = static_cast<QPushButton*> (parentWidget());
+    if (parent)
+        parent->setChecked(false);
+}
+
---
+++ PianoBooster/src/GuiLoopingPopup.h
@@ -0,0 +1,59 @@
+/*********************************************************************************/
+/*!
+@file           GuiLoopingPopup.h
+
+@brief          xxxx.
+
+@author         L. J. Barman
+
+    Copyright (c)   2008, L. J. Barman, all rights reserved
+
+    This file is part of the PianoBooster application
+
+    PianoBooster is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PianoBooster is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PianoBooster.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/*********************************************************************************/
+
+#ifndef __GUILOOPINGPOPUP_H__
+#define __GUILOOPINGPOPUP_H__
+
+
+#include <QtGui>
+
+#include "Song.h"
+
+
+#include "ui_GuiLoopingPopup.h"
+
+class CGLView;
+
+class GuiLoopingPopup : public QWidget, private Ui::GuiLoopingPopup
+{
+    Q_OBJECT
+
+public:
+    GuiLoopingPopup(QWidget *parent = 0);
+    void init(CSong* song);
+
+private slots:
+    void on_loopBarsSpin_valueChanged(double bars);
+    void closeEvent(QCloseEvent *event);
+
+private:
+    void updateInfo();
+    CSong* m_song;
+};
+
+#endif //__GUILOOPINGPOPUP_H__
