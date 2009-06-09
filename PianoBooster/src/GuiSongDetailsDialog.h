9c9c27f8d6db59626dd0a4570323cb09a5592e34
louisjb <louisjb@ba081f5d-443b-49a7-ac4b-446c3f91f371>
2009-06-09 19:54:39 +0000

    song
    git-svn-id: https://svn.code.sf.net/p/pianobooster/code/trunk@63 ba081f5d-443b-49a7-ac4b-446c3f91f371

---
+++ PianoBooster/src/GuiSongDetailsDialog.h
@@ -0,0 +1,60 @@
+/*********************************************************************************/
+/*!
+@file           GuiSongDetailsDialog.h
+
+@brief          xxxx.
+
+@author         L. J. Barman
+
+    Copyright (c)   2008-2009, L. J. Barman, all rights reserved
+
+    This file is part of the PianoBooster application
+
+    PianoBooster is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PianoBooster is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PianoBooster.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/*********************************************************************************/
+
+#ifndef __GUISONGDETAILSDIALOG_H__
+#define __GUISONGDETAILSDIALOG_H__
+
+
+#include <QtGui>
+
+#include "Song.h"
+#include "Settings.h"
+
+
+#include "ui_GuiSongDetailsDialog.h"
+
+class CGLView;
+
+class GuiSongDetailsDialog : public QDialog, private Ui::GuiSongDetailsDialog
+{
+    Q_OBJECT
+
+public:
+    GuiSongDetailsDialog(QWidget *parent = 0);
+    void init(CSong* song, CSettings* settings, CGLView* glView);
+
+private slots:
+    void accept();
+
+private:
+    CSettings* m_settings;
+    CSong* m_song;
+    CGLView *m_glView;
+};
+
+#endif //__GUISONGDETAILSDIALOG_H__
