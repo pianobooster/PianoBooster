db88357afd06c04c5f41798485903a06741e5336
Fabien Givors <fabien.givors@chezlefab.net>
2018-05-21 18:36:11 +0200

    Fix: filename conversion->toLocal8Bit

--- src/Song.cpp
+++ src/Song.cpp
@@ -57,8 +57,8 @@ void CSong::loadSong(const QString & filename)
      fn = fn.replace('/','\\');
 #endif
     m_midiFile->setLogLevel(3);
-    m_midiFile->openMidiFile(string(fn.toLatin1()));
-    ppLogInfo("Opening song %s",  string(fn.toLatin1()).c_str());
+    m_midiFile->openMidiFile(string(fn.toLocal8Bit().data()));
+    ppLogInfo("Opening song %s",  fn.toLocal8Bit().data());
     transpose(0);
     midiFileInfo();
     m_midiFile->setLogLevel(99);
