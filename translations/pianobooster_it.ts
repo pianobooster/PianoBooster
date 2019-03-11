ac5eb4c7b3d8f5bff8137d218434b923e2b081d1
glixx <roman_romul@mail.ru>
2019-03-11 22:10:35 +0000

    Translated using Weblate (Italian)
    Currently translated at 51.9% (188 of 362 strings)
    
    Translation: Piano Booster/Translations
    Translate-URL: https://hosted.weblate.org/projects/piano-booster/translations/it/

--- translations/pianobooster_it.ts
+++ translations/pianobooster_it.ts
@@ -1796,7 +1796,7 @@
     <message>
         <location filename="../src/QtWindow.cpp" line="603"/>
         <source>space</source>
-        <translation type="unfinished">Spazio</translation>
+        <translation>Spazio</translation>
     </message>
     <message>
         <location filename="../src/QtWindow.cpp" line="616"/>
