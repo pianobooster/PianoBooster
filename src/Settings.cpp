fea07f0fefc686743ec58d7cd8c7173bbafd91e5
sasha <dik@inbox.ru>
2019-07-08 10:03:04 +0900

    change default for Follow Through Errors

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -68,7 +68,7 @@ CSettings::CSettings(QtWindow *mainWindow) : QSettings(CSettings::IniFormat, CSe
     m_noteNamesEnabled = value("Score/NoteNames", true ).toBool();
     m_tutorPagesEnabled = value("Tutor/TutorPages", true ).toBool();
     CNotation::setCourtesyAccidentals(value("Score/CourtesyAccidentals", false ).toBool());
-    m_followThroughErrorsEnabled = value("Score/FollowThroughErrors", true ).toBool();
+    m_followThroughErrorsEnabled = value("Score/FollowThroughErrors", false ).toBool();
 }
 
 void CSettings::setDefaultValue(const QString & key, const QVariant & value )
